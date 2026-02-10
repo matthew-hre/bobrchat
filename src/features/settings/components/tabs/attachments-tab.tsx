e942f01a2f492d0cea69f91c271aeac67ad59611
Matthew Hrehirchuk <me@matthew-hre.com>
2026-02-09 21:34:03 -0700

    attachments: fix overflow in settings

--- src/features/settings/components/tabs/attachments-tab.tsx
+++ src/features/settings/components/tabs/attachments-tab.tsx
@@ -383,12 +383,14 @@ export function AttachmentsTab() {
                           rel="noopener noreferrer"
                           target="_blank"
                           className={`
-                            flex flex-row items-center truncate text-sm
+                            flex flex-row items-center
                             hover:underline
                           `}
                           title={item.filename}
                         >
+                        <span className="text-sm max-w-80 truncate">
                           {item.filename}
+                          </span>
                           <ExternalLinkIcon className={`
                             text-muted-foreground ml-1 inline-block size-3
                           `}
