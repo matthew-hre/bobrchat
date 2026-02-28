d72c88351f56871243d874f3f1292d1792d5ceb9
Matthew Hrehirchuk <me@matthew-hre.com>
2026-02-27 21:40:08 -0700

    chat: fix auth issue (#151)

--- src/proxy.ts
+++ src/proxy.ts
@@ -33,6 +33,6 @@ export default async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon\\.ico|.*\\.png$).*)",
+    "/((?!_next/static|_next/image|favicon\\.ico|.*\\.png$).*)",
   ],
 };
